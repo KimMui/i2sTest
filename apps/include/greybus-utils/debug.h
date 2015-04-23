c5b8f248443130fb00b40a8189ec2a4bdc231fa8
Mark A. Greer <mgreer@animalcreek.com>
2015-04-22 18:20:25 -0700

    XXX: Make gb_info() subject to CONFIG_GREYBUS_DEBUG
    Testing Done:
    Bug Number:
    Review URL:
    Reviewed by:
    
    Signed-off-by: Mark A. Greer <mgreer@animalcreek.com>

--- apps/include/greybus-utils/debug.h
+++ apps/include/greybus-utils/debug.h
@@ -51,18 +51,23 @@ static inline void _gb_dump(const char *func, __u8 *buf, size_t size)
         _gb_dump(__func__, buf, size)
 #define gb_debug(fmt, ...) \
         do { lowsyslog("[D] GB: " fmt, ##__VA_ARGS__); } while (0)
-#else
-#define gb_dump(buf, size) \
-        do { } while (0)
-#define gb_debug(fmt, ...) \
-        do { } while (0)
-#endif
-
 /* debug/info/error macros */
 #define gb_info(fmt, ...)                                            \
         do { lowsyslog("[I] GB: " fmt, ##__VA_ARGS__); } while (0)
 #define gb_error(fmt, ...)                                           \
         do { lowsyslog("[E] GB: " fmt, ##__VA_ARGS__); } while (0)
+
+#else
+#define gb_dump(buf, size) \
+        do { } while (0)
+#define gb_debug(fmt, ...) \
+        do { } while (0)
+/* debug/info/error macros */
+#define gb_info(fmt, ...)                                            \
+        do { } while (0)
+#define gb_error(fmt, ...)                                           \
+        do { } while (0)
+#endif
 
 #endif
 
